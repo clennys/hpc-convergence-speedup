c6c9cfa9b269e5fed66cd6fec31c2f075c1b9842
Dennys Huber <dennys.huber@proton.me>
2023-05-02 22:10:37 +0200

    update

--- experiment.sh
+++ experiment.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-for i in {2..13}; do
-	for j in {5..40}; do
-		echo "Processes: $i with grid dim $j"
-		mpirun -np $i ./build/apps/main -p -dj -fps -gd $j >> damped_jacobi.log
-		mpirun -np $i ./build/apps/main -p -gs -fps -gd $j >> gauss_seidel.log
+for i in {2..64}; do
+	for j in {3..67}; do
+	dim=$((j - 1))
+        square=$((dim * dim))
+        if [ "$square" -ge "$i" ]; then
+			echo "[Processes: $i/64] :: Grid dim = $j/67 :: Matrix dim = $square"
+			mpirun -np $i ./build/apps/main -p -dj -fps -gd $j >> damped_jacobi.log
+			mpirun -np $i ./build/apps/main -p -gs -fps -gd $j >> gauss_seidel.log
+		fi
 	done
 done
-
-# ./build/apps/main -s -dj -fps -gd 11
-# ./build/apps/main -s -gs -fps -gd 11
--- src/iterative_solvers/parallel.cc
+++ src/iterative_solvers/parallel.cc
@@ -44,7 +44,7 @@ void run_parallel(string solver, string x_point_formula, int grid_size) {
     }
 
     // print_matrix(matrix, p.matrix_dim);
-    print_vector(b, p.matrix_dim);
+    // print_vector(b, p.matrix_dim);
   }
 
   double start = MPI_Wtime();
