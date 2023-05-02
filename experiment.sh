03070c874e548508b34edb5b0f25080a7077f267
Dennys Huber <dennys.huber@proton.me>
2023-05-02 22:37:14 +0200

    another improvement to the shell script

--- experiment.sh
+++ experiment.sh
@@ -7,8 +7,8 @@ for i in {2..64}; do
         square=$((dim * dim))
         if [ "$square" -ge "$i" ]; then
 			echo "[Processes: $i/64] :: Grid dim = $j/67 :: Matrix dim = $square"
-			mpirun -np $i ./build/apps/main -p -dj -fps -gd $j > output/"damped_jacobi_$i.$square.$current_time.log"
-			mpirun -np $i ./build/apps/main -p -gs -fps -gd $j > output/"gauss_seidel$i.$square.$current_time.log"
+			mpirun -np $i ./build/apps/main -p -dj -fps -gd $j > output/"damped_jacobi.$i.$square.$current_time.log"
+			mpirun -np $i ./build/apps/main -p -gs -fps -gd $j > output/"gauss_seidel.$i.$square.$current_time.log"
 		fi
 	done
 done
