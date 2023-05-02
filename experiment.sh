f985a95ab8cef87df1c01950fe29aae1902eac96
Dennys Huber <dennys.huber@proton.me>
2023-05-02 22:20:00 +0200

    updated shell script

--- experiment.sh
+++ experiment.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 
 for i in {2..64}; do
 	for j in {3..67}; do
-	dim=$((j - 1))
+		dim=$((j - 1))
         square=$((dim * dim))
         if [ "$square" -ge "$i" ]; then
 			echo "[Processes: $i/64] :: Grid dim = $j/67 :: Matrix dim = $square"
-			mpirun -np $i ./build/apps/main -p -dj -fps -gd $j >> "damped_jacobi_$i_$dim.log"
-			mpirun -np $i ./build/apps/main -p -gs -fps -gd $j >> "gauss_seidel$i_$square.log"
+			mpirun -np $i ./build/apps/main -p -dj -fps -gd $j > output/"damped_jacobi_$i.$square.$current_time.log"
+			mpirun -np $i ./build/apps/main -p -gs -fps -gd $j > output/"gauss_seidel$i.$square.$current_time.log"
 		fi
 	done
 done
