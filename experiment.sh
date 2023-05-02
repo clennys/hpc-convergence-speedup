a7e6caa6f47c55f2a253d9cf643a9c245dcf4868
Dennys Huber <dennys.huber@proton.me>
2023-05-02 21:04:00 +0200

    updated shell script

--- experiment.sh
+++ experiment.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 for i in {2..13}; do
-	mpirun -np $i ./build/apps/main -p -dj -fps -gd 11
-	mpirun -np $i ./build/apps/main -p -gs -fps -gd 11
+	for j in {5..40}; do
+		echo "Processes: $i with grid dim $j"
+		mpirun -np $i ./build/apps/main -p -dj -fps -gd $j >> damped_jacobi.log
+		mpirun -np $i ./build/apps/main -p -gs -fps -gd $j >> gauss_seidel.log
+	done
 done
 
-./build/apps/main -s -dj -fps -gd 11
-./build/apps/main -s -gs -fps -gd 11
+# ./build/apps/main -s -dj -fps -gd 11
+# ./build/apps/main -s -gs -fps -gd 11
